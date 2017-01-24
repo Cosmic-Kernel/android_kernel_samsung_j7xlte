5894269e12ed84ac6e0c1ccde64eccfca03e7794
alexax66 <alexax66@mail.ru>
2017-01-24 12:59:33 +0000

    cpufreq: cpu-boost: Add cpu-boost driver When certain bursty and important events take place, it might take a while for the current cpufreq governor to notice the new load and react to it. That would result in poor user experience. To alleviate this, the cpu-boost driver boosts the frequency of a CPU for a short duration to maintain good user experience while the governor catches up.
    Specifically, this commit deals with ensuring that when "important" tasks
    migrate from a fast CPU to a slow CPU, the frequency of the slow CPU is
    boosted to be at least as high as the fast CPU for a short duration.
    
    Since this driver enforces the boost by hooking into standard cpufreq
    ADJUST notifiers, it has several advantages:
    - More portable across kernel versions where the cpufreq internals might
      have been rewritten.
    - Governor agnostic and hence works with multiple governors like
      conservative, ondemand, interactive, etc.
    - Does not affect the sampling period/logic of existing governors.
    - Can have the boost period adjusted independent of governor sampling
      period.
    
    Change-Id: Ibd814a20043d0aba64ee7637a4a79b9ffa1b0991
    Signed-off-by: Saravana Kannan <skannan@codeaurora.org>
    
    Adapted by Alexander Alexeev <alexax66@gmail.com>
    
    Based on commits: https://github.com/akhilnarang/ThugLife_bullhead/commits/0ab0ce974c13acbd05cce657228d6b4d10ab8d5e/drivers/cpufreq/cpu-boost.c
    Used patch: https://github.com/moddingg33k/android_kernel_y300_general/blob/master/patches/kernel_sched_provide_per_cpu_cgroup_option
    
    Conflicts:
    	drivers/cpufreq/Makefile

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4990,6 +4990,8 @@ struct lb_env {
 	unsigned int		loop_max;
 };
 
+static DEFINE_PER_CPU(bool, dbs_boost_needed);
+
 /*
  * move_task - move a task from one runqueue to another runqueue.
  * Both runqueues must be locked.
@@ -5000,6 +5002,8 @@ static void move_task(struct task_struct *p, struct lb_env *env)
 	set_task_cpu(p, env->dst_cpu);
 	activate_task(env->dst_rq, p, 0);
 	check_preempt_curr(env->dst_rq, p, 0);
+	if (task_notify_on_migrate(p))
+		per_cpu(dbs_boost_needed, env->dst_cpu) = true;
 }
 
 /*
@@ -6341,9 +6345,20 @@ more_balance:
 			 */
 			sd->nr_balance_failed = sd->cache_nice_tries+1;
 		}
-	} else
+	} else {
 		sd->nr_balance_failed = 0;
+		if (per_cpu(dbs_boost_needed, this_cpu)) {
+			struct migration_notify_data mnd;
 
+			mnd.src_cpu = cpu_of(busiest);
+			mnd.dest_cpu = this_cpu;
+			if (mnd.load > 100)
+				mnd.load = 100;
+			atomic_notifier_call_chain(&migration_notifier_head,
+						    0, (void *)&mnd);
+			per_cpu(dbs_boost_needed, this_cpu) = false;
+		}
+	}
 	if (likely(!active_balance)) {
 		/* We were unbalanced, so reset the balancing interval */
 		sd->balance_interval = sd->min_interval;
@@ -6505,6 +6520,18 @@ static int active_load_balance_cpu_stop(void *data)
 out_unlock:
 	busiest_rq->active_balance = 0;
 	raw_spin_unlock_irq(&busiest_rq->lock);
+	if (per_cpu(dbs_boost_needed, target_cpu)) {
+		struct migration_notify_data mnd;
+
+		mnd.src_cpu = cpu_of(busiest_rq);
+		mnd.dest_cpu = target_cpu;
+		if (mnd.load > 100)
+			mnd.load = 100;
+		atomic_notifier_call_chain(&migration_notifier_head,
+					    0, (void *)&mnd);
+
+		per_cpu(dbs_boost_needed, target_cpu) = false;
+	}
 	return 0;
 }
 
