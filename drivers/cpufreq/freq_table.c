4632c19cb5fb7e5b745d8878c02045d11d0badb5
Viresh Kumar <viresh.kumar@linaro.org>
2017-05-01 18:32:28 +0000

    cpufreq: Optimize cpufreq_frequency_table_verify()
    cpufreq_frequency_table_verify() is rewritten here to make it more logical
    and efficient.
     - merge multiple lines for variable declarations together.
     - quit early if any frequency between min/max is found.
     - don't call cpufreq_verify_within_limits() in case any valid freq is
       found as it is of no use.
     - rename the count variable as found and change its type to boolean.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    
    Used commit: https://github.com/Noxxxious/Zero/commit/a04a517aa911804b26f5970e49d48d35e1a62b24

--- drivers/cpufreq/freq_table.c
+++ drivers/cpufreq/freq_table.c
@@ -1,3 +1,4 @@
+
 /*
  * linux/drivers/cpufreq/freq_table.c
  *
@@ -79,9 +80,8 @@ int cpufreq_frequency_table_verify(struct cpufreq_policy *policy,
 
 	if (!found) {
 		policy->max = next_larger;
-
 		cpufreq_verify_within_limits(policy, policy->cpuinfo.min_freq,
-						policy->cpuinfo.max_freq);
+				policy->cpuinfo.max_freq);
 	}
 
 	pr_debug("verification lead to (%u - %u kHz) for cpu %u\n",
